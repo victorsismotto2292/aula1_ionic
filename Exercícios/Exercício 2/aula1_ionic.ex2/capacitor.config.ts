66fee225865926c9eb9f88edb1822663064276fb
Vecyn_SisMoTTO <167320276+victorsismotto2292@users.noreply.github.com>
2026-02-10 13:35:57 -0300

    First commit - files
    Exercises folder commit and aula1 folder commit

---
+++ Exercícios/Exercício 2/aula1_ionic.ex2/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'aula1_ionic.ex2',
+  webDir: 'www'
+};
+
+export default config;
