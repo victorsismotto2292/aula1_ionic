7f69762b5d1e7900a952c06ce6f389b5acdf7459
Vecyn_SisMoTTO <vgr.sismotto@gmail.com>
2026-02-10 21:32:20 -0300

    Arrumando pastas
    Excluíndo pastas dos exercícios temporariamente

--- Exercícios/Exercício 1/aula1_ionic.ex1/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'aula1_ionic.ex1',
-  webDir: 'www'
-};
-
-export default config;
