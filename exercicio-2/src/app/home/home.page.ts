4fab65cde61280e423535322c0a063183e3cbf62
Vecyn_SisMoTTO <vgr.sismotto@gmail.com>
2026-02-10 22:01:57 -0300

    Exercício 2 funcionando
    FINAL COMMIT

--- exercicio-2/src/app/home/home.page.ts
+++ exercicio-2/src/app/home/home.page.ts
@@ -10,4 +10,13 @@ export class HomePage {
 
   constructor() {}
 
+  AC='';
+  AG='';
+  AT='';
+  res = 0;
+
+  media(){
+    this.res = ((parseFloat(this.AC)*0.2) + (parseFloat(this.AG)*0.10) + (parseFloat(this.AT)*0.70))/3;
+  }
+
 }
